1374cd93b746b1ea63f3329c9ac8d73b4ae7e029
ant <38507580+antch57@users.noreply.github.com>
2023-11-26 19:03:20 -0700

    table driven testing / refactors

--- geometry/geometry_test.go
+++ geometry/geometry_test.go
@@ -13,11 +13,20 @@ func TestPerimeter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
 
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
+	areaTests := []struct {
+		shape Shape
+		want  float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("%#v got %g, want %g", tt.shape, got, tt.want)
+		}
 	}
 }
